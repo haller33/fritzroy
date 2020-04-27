e1a09622032912e2caaecaf5db659fa18b242bd8
haller218 <eliseu42lucena@gmail.com>
2020-04-27 15:08:37 -0300

    separete repl to main MCE

---
+++ repl.evil.scm
@@ -0,0 +1,171 @@
+#! /nix/store/jw2522hjypr3dv8v2sjk8gmk4jywi43w-user-environment/bin/scheme --script
+;;#!/data/data/com.termux/files/usr/bin/guile -s
+;;!#
+
+
+;;;
+;; this is my own definition of Meta Circular Evaluator.
+;;
+
+(define (inenv? exp env)
+  (define (ienvaps env acc)
+    (if (or (null? env) acc)
+	acc
+	(ienvaps (cdr env)
+		 (or (eqv? (caar env) exp)
+		     acc))))
+  (ienvaps env #f))
+
+
+(define (extraenv exp env)
+  (if (eqv? (caar env) exp)
+      (cadar env)
+      (extraenv exp (cdr env))))
+
+
+(define (insidenv frame env)
+  (if (or (null? frame) (number? frame))
+      env
+      (insidenv (cdr frame)
+		(cons (car frame)
+		      env))))
+
+(define (outsidenv frame env)
+  (if (or (null? env) (number? frame))
+      env
+      (insidenv (cdr frame)
+		(cdr env))))
+
+(define (gc env)
+  (define (exist? item lst)
+    (and (not (null? lst))
+	 (or (eqv? item (caar lst))
+	     (exist? item (cdr lst)))))
+  (define (gc-aps env acc)
+    (if (null? env)
+	(reverse acc)
+	(gc-aps (cdr env)
+		(if (not (exist? (caar env) acc))
+		    (cons (car env) acc)
+		    acc))))
+  (gc-aps env (list (car env))))
+
+(define (dothis exp env)
+  ;;(if (atom? exp))) ;; TODO
+  '())
+      
+  
+(define (evale exp env)
+  (cond ((eqv? exp 'nil)
+	 '())
+	((eqv? exp '#f)
+	 #f)
+	((eqv? exp '#t)
+	 #t)
+	((string? exp)
+	 exp)
+        ((null? exp)
+	 'nil)
+	((number? exp)
+	 exp)
+	
+	((inenv? exp env)
+	 (evale (extraenv exp env) env))
+	((eqv? (car exp) 'do) ;; TODO
+	 (dothis (list (cdr exp)) env))
+	((eqv? (car exp) 'set)
+	 (evale (caddr exp)
+		(cons (list (cadr exp)
+			    (evale (caddr exp) env))
+		      env)))
+	((eqv? (car exp) 'let)
+	 (evale (caddr exp)
+		(insidenv (cadr exp) env))
+	 (evale '() (outsidenv (cadr exp) env)))
+	((eqv? (car exp) 'lambda)
+	 (cons '<PROCEDURE>
+	       (list (caddr exp)
+		     (cadr exp))))
+	((eqv? (car exp) 'quot)
+	 exp)
+	((eqv? (car exp) 'eval)
+	 (evale (cadadr exp) '()))
+	((eqv? (car exp) 'write)
+	 (write	(evale (cadr exp) env))
+	 'nil)
+	((eqv? (car exp) '==)
+	 (eqv? (evale (cadr exp) env)
+	       (evale (caddr exp) env)))
+	((eqv? (car exp) '>)
+	 (> (evale (cadr exp) env)
+	    (evale (caddr exp) env)))	
+	((eqv? (car exp) '<)
+	 (< (evale (cadr exp) env)
+	    (evale (caddr exp) env)))
+	((eqv? (car exp) 'sub1)
+	 (- (evale (caddr exp) env)
+	    (evale (cadr exp) env)))
+	((eqv? (car exp) 'some1)
+	 (+ (evale (cadr exp) env)
+	    (evale (caddr exp) env)))
+	((eqv? (car exp) 'mult1)
+	 (* (evale (cadr exp) env)
+	    (evale (caddr exp) env)))
+	((eqv? (car exp) 'if)
+	 (if (evale (cadr exp) env)
+	     (evale (caddr exp) env)))
+	((eqv? (car exp) 'cons)
+	 (cons (evale (cadr exp) env)
+	       (evale (caddr exp) env)))
+	((eqv? (car exp) '\,env)
+	 (write env)
+	 (evale 'nil env))
+	((eqv? (car exp) '\,gc)
+	 (evale '(quot gc) (gc env)))
+	((eqv? (caar env) 'exit)
+	 (format #t "goodbye~%"))
+	((eqv? (car exp) '\,repl)
+	 (write (cadar env))
+	 (format #t "~% evil > ")
+	 (evale '(\,repl) (cons (list 'repl (read))
+				(cdr env))))
+	
+	((and (symbol? (car exp))
+	      (symbol? (cadr exp))
+	      (null? (cddr exp)))
+	 (applye (list (evale (car exp) env)
+		       (evale (cdr exp) env))))
+
+	(#t
+	 (write 'error)
+	 (write exp)
+	 (write env)
+	 'nil)))
+
+
+(define (applye exp env)
+  (cond ((and (eqv? (caaar exp) '<PROCEDURE>)
+	     (eqv? (cadar exp) '<PROCEDURE>))
+	 (cons 'let
+	       (list exp)))))
+		 
+
+
+;; debug mode
+(trace evale)
+(trace inenv?)
+(trace extraenv)
+(trace insidenv)
+(trace outsidenv)
+(trace gc)
+(trace applye)
+
+;;(let ((n
+;;       (evale '()
+;;	      '((j 88)(n 5)))))
+;;  (format #t "~A~%" n))
+
+
+
+
+(evale '(\,repl) '((repl Meta-Circular-Evaluator)))
