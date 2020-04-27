43056f278301df0c12e29c118eb200ee6e327585
haller218 <eliseu42lucena@gmail.com>
2020-04-26 21:33:20 -0300

    my full lambda lang: set, let, enviropment

--- handmade.scm
+++ handmade.scm
@@ -1,4 +1,5 @@
-#! /nix/store/n4a0i8qpi20cyrd2hfvpnwx93kphy7qg-user-environment/bin/scheme --script
+#!/data/data/com.termux/files/usr/bin/guile -s
+!#
 
 ;;;
 ;; this is my own definition of Meta Circular Evaluator.
@@ -14,6 +15,18 @@
   (ienvaps exp env #f))
 
 
+(define (extraenv exp env)
+  (if (eqv? (caar env) exp)
+      (cadar env)
+      (extraenv exp (cdr env))))
+
+(define (insidenv add env)
+  (if (null? add)
+      env
+      (insidenv (cdr add)
+		(cons (car add)
+		      env))))
+
 (define (evale exp env)
   (cond ((eqv? exp 'nil)
 	 '())
@@ -26,7 +39,15 @@
 	((number? exp)
 	 exp)
 	((inenv? exp env)
-	 (extraenv (evale exp env)))
+	 (evale (extraenv exp env) env))
+	((eqv? (car exp) 'set)
+	 (evale (caddr exp)
+		(cons (cons (cadr exp)
+			    (evale (caddr exp) env))
+		      env)))
+	((eqv? (car exp) 'let)
+	 (evale (caddr exp)
+		(insidenv (cadr exp) env)))
 	((eqv? (car exp) 'quot)
 	 exp)
 	((eqv? (car exp) '==)
@@ -57,12 +78,15 @@
 
 
 ;; debug mode
-(trace evale)
+;; (trace evale)
+
 
 (let ((n
-
-       (evale 'n
-	      '((n 5)))))
+       
+       (evale '(let ((x 7))
+		 (let ((h n))
+		   (sub1 h x)))
+	      '((j 88)(n 5)))))
   
   (format #t "~A~%" n))
 
