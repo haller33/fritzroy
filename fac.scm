9d4df7bf4443b6d766dd3681920e3aa9990e1ec9
CHANGEME <CHANGEME>
2023-12-04 21:44:00 -0300

    new changes, from a long time a go of winter to discovery of Category Theory and Co-Reader-Monad

--- fac.scm
+++ fac.scm
@@ -1,14 +1,15 @@
 (load "fritzroy.scm")
 
-(let ((n (eval-expr '(((lambda (!)
-			 (lambda (n)
-			   ((! !) n)))
-		       (lambda (!)
-			 (lambda (n)
-			   (if (zero? n)
-			       1
-			       (* n ((! !) (sub1 n)))))))
-		      5)
-		    (lambda(x)
-		      (error 'lookup "envirompment")))))
+(let ((n (eval-expr
+		  '(((lambda (!)
+			   (lambda (n)
+				 ((! !) n)))
+		     (lambda (!)
+			   (lambda (n)
+				 (if (zero? n)
+					 1
+					 (* n ((! !) (sub1 n)))))))
+		    5)
+		  (lambda(x)
+		    (error 'lookup "envirompment")))))
   (format #t "Factorial of ~a is : ~a~%" 5 n))
